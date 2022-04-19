d535ea0d92784dbd64a5450f1756873efcb022e1
Szymon Jasinski <szymon@szymi.com>
2022-04-19 22:40:29 +0200

    initial commit

---
+++ src/huawei_to_influx.py
@@ -0,0 +1,73 @@
+from huawei_solar import HuaweiSolar
+from datetime import datetime
+from influxdb import InfluxDBClient
+
+import settings
+import time
+
+
+class HuaweiToInflux:
+    def __init__(self):
+        self.huawei = HuaweiSolar(settings.HUAWEI_ADDRESS, slave=settings.HUAWEI_SLAVE)
+        self.influx = InfluxDBClient(
+            host=settings.INFLUX_HOST, port=settings.INFLUX_PORT, 
+            username=settings.INFLUX_USERNAME, password=settings.INFLUX_PASSWORD, ssl=False
+        )
+        self.influx.switch_database(settings.INFLUX_DATABASE)
+
+
+    def get_huawei_solar_data(self):
+        print("Starting to get solar data...")
+        while True:
+            device_status = self.huawei.get('device_status')
+            self.write_point('device_status', device_status.value)
+            if device_status.value != 'On-grid':
+                print("Waiting for \"On-grid\" status...")
+                break
+            for key in settings.KEYS:
+                if key == 'device_status':
+                    continue
+                result = self.huawei.get(key)
+                if type(result.value) == datetime:
+                    value = result.value.isoformat()
+                else:
+                    value = result.value
+                point = self.write_point(key, value)
+
+        print("Sleeping 30 seconds...")
+        time.sleep(30)
+        self.get_huawei_solar_data()
+
+
+    def write_point(self, key, value):
+        point = [
+            {
+                "measurement": "huawei",
+                "time": datetime.now().isoformat(),
+                "fields": {
+                    key: value
+                }
+            }
+        ]
+        print("Writing measurement:")
+        print(point)
+        self.influx.write_points(point)
+
+
+
+def get_huawei_solar_data():
+    while True:
+        for key in settings.KEYS:
+            result = huawei.get(key)
+            if type(result.value) == datetime:
+                value = result.value.isoformat()
+            else:
+                value = result.value
+            point = get_point(key, value)
+            client.write_points(point)
+            print("{} ({})".format(point, result.unit))
+
+
+if __name__ == "__main__":
+    huawei_to_influx = HuaweiToInflux()
+    huawei_to_influx.get_huawei_solar_data()
---
+++ src/settings.py
@@ -0,0 +1,43 @@
+import os
+
+
+HUAWEI_ADDRESS = os.environ.get('HUAWEI_ADDRESS')
+HUAWEI_SLAVE = int(os.environ.get('HUAWEI_SLAVE'))
+INFLUX_HOST = os.environ.get('INFLUX_HOST')
+INFLUX_PORT = os.environ.get('INFLUX_PORT')
+INFLUX_USERNAME = os.environ.get('INFLUX_USERNAME')
+INFLUX_PASSWORD = os.environ.get('INFLUX_PASSWORD')
+INFLUX_DATABASE = os.environ.get('INFLUX_DATABASE')
+
+KEYS = [
+    "pv_01_voltage",
+    "pv_02_voltage",
+    "pv_01_current",
+    "pv_02_current",
+    "input_power",
+    "grid_voltage",
+    "line_voltage_A_B",
+    "line_voltage_B_C",
+    "line_voltage_C_A",
+    "phase_A_voltage",
+    "phase_B_voltage",
+    "phase_C_voltage",
+    "grid_current",
+    "phase_A_current",
+    "phase_B_current",
+    "phase_C_current",
+    "day_active_power_peak",
+    "active_power",
+    "reactive_power",
+    "power_factor",
+    "grid_frequency",
+    "efficiency",
+    "internal_temperature",
+    "insulation_resistance",
+    "device_status",
+    "startup_time",
+    "shutdown_time",
+    "fault_code",
+    "accumulated_yield_energy",
+    "daily_yield_energy",
+]
